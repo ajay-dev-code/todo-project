b32854dab43c28860e96cc86f287b861ac284a88
ajay-dev-code <ajayajay92004@gmail.com>
2026-08-19 15:25:09 +0530

    Remove old mysql package and bin folder

--- com/example/mysql/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mysql.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mysql.Entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
